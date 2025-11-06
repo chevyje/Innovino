e6f3909ee2c6a49570e0f5abd02f659464fea96a
chevyje <jurre.blankers@gmail.com>
2025-11-06 12:46:56 +0100

    Added POST request for login handling

--- frontend/src/loginPagina/login.tsx
+++ frontend/src/loginPagina/login.tsx
@@ -2,9 +2,10 @@ import Style from "./login.module.css"
 import bg from "../assets/inlog-background.jpg"
 
 export default function loginPage() {
-    function login (formData: any){
+    function loginSubmit (formData: any){
         const username: string = formData.get('username')
-        alert(username)
+        const password: string = formData.get('password')
+        auth({ username: username, password: password }).then(() => console.log('login tried'))
     }
     return (
         <>
@@ -12,7 +13,7 @@ export default function loginPage() {
                 <div className={Style.wrapper}>
                     <h1 className={Style.title}>Inloggen</h1>
                     <div className={Style.loginContainer}>
-                        <form action={login} className={Style.loginForm}>
+                        <form action={loginSubmit} className={Style.loginForm}>
                             <p className={Style.tag}>Gebruikersnaam</p>
                             <input type={"text"} name={"username"} className={Style.inputField} />
                             <p className={Style.tag}>Wachtwoord</p>
@@ -24,4 +25,21 @@ export default function loginPage() {
             </div>
         </>
     )
+}
+interface User {
+    username: string
+    password: string
+}
+function auth (user: User) {
+    const headers: Headers = new Headers()
+    headers.set("Content-Type", "application/json")
+    headers.set("Accept", "application/json")
+
+    const request: RequestInfo = new Request('http://127.0.0.1:8000/api/users/auth', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(user)
+    })
+
+    return fetch(request).then(res => res.json()).then((data) => alert(JSON.stringify(data)))
 }
