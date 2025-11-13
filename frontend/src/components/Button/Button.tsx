b9ed3abd6c0fef29b840b57691e9844a57408e40
tobia <tobiaslbeumer@gmail.com>
2025-11-13 13:32:04 +0100

    Added button functionality

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -3,15 +3,16 @@ import Style from "./button.module.css"
 interface Props {
     title?: string,
     variant?: string,
+    action?: any;
 }
 
-export default function Button({ title, variant }: Props) {
+export default function Button({ title, variant, action }: Props) {
     const btnVariant : string = variant ?? "primary";
     const btnTitle : string = title ?? "Button"
     const classes = `${Style.button} ${Style[btnVariant]}`;
     return (
         <div className={Style.buttonContainer}>
-            <button className={`${classes}`}>{btnTitle}</button>
+            <button className={`${classes}`} onClick={action}>{btnTitle}</button>
         </div>
     );
 }
--- frontend/src/testPage/test.tsx
+++ frontend/src/testPage/test.tsx
@@ -4,7 +4,7 @@ import Button from "../components/Button/Button.tsx";
 export default function TestPage() {
     return (
         <div className={Style.container}>
-            <Button title={"Button"}></Button>
+            <Button action={() => alert("TEST")}></Button>
             <Button title={"Button"} variant={"secondary"}></Button>
             <Button title={"Button"} variant={"tertiary"}></Button>
         </div>
