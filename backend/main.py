6030146d19949cd3d8ac640222218e3c03978755
chevyje <jurre.blankers@gmail.com>
2025-11-19 10:06:28 +0100

    Forgot to restructure the api.py. This is now main.py and is restructured.

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,10 @@
-from contextlib import asynccontextmanager
-from pydantic import BaseModel
-from typing import List
+from backend.routers.user_router import router as user_router
+from backend.database.database import connect_db
+from backend.services.security_service import handle_api_key_middleware
 
-# Fast API
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-
-# Routers
-from backend.routers.users import router as users_router
-
-# Internal codes
-from database import connect_db
-from backend.services.session import check_session
-
-class ApiKeyException(BaseModel):
-    path: str
-    method: str
-
-key_exceptions: List[ApiKeyException] = [
-    ApiKeyException(method="POST", path="/api/users/auth"),
-    
-    # Allow the docs to be read
-    ApiKeyException(method="GET", path="/docs"),
-    ApiKeyException(method="GET", path="/openapi.json")
-]
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -36,19 +16,7 @@ app = FastAPI(lifespan=lifespan)
 
 @app.middleware("http")
 async def check_api_key(request: Request, call_next):
-    # Check if route is in the exception and continue
-    for key_exception in key_exceptions:
-        if key_exception.method == request.method and key_exception.path == request.url.path:
-            print("No API key needed")
-            return await call_next(request)
-
-    # Check if the api key is valid
-    api_key = request.headers.get("x-api-key")
-    if check_session(api_key):
-        print("Key is valid")
-        return await call_next(request)
-    else:
-        return JSONResponse(status_code=401, content={"message": "Invalid API key"})
+    return handle_api_key_middleware(request, call_next)
 
 app.add_middleware(
     CORSMiddleware,
@@ -58,4 +26,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(users_router, prefix="/api")
+app.include_router(user_router, prefix="/api")
